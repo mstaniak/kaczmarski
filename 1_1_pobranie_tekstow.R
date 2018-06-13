115cf18adcb5badd21a1d47ce365481bbda5e368
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2018-06-13 10:46:00 +0200

    polaczenie tytulow, lat i wierszy, nowe slowa stopu

--- 1_1_pobranie_tekstow.R
+++ 1_1_pobranie_tekstow.R
@@ -1,24 +1,23 @@
+# Biblioteki. ----
 library(tidyverse)
 library(rvest)
 library(stringr)
-library(wordcloud)
-
+# Linki do wierszy ----
 strona_glowna <- read_html("http://www.kaczmarski.art.pl/tworczosc/wiersze/")
 lista_linkow <- strona_glowna %>%
   html_node(xpath = '//*[@id="post-2747"]/div/div/div/div/div/ul') %>%
   html_nodes("a") %>%
   html_attr("href")
 save(lista_linkow, file = "lista_linkow.rda")
-
+# Tytuły wierszy ----
 tytuly <- strona_glowna %>%
   html_node(xpath = '//*[@id="post-2747"]/div/div/div/div/div/ul') %>%
   html_nodes("a") %>%
   html_text()
 save(tytuly, file = "tytuly.rda")
-
+# Wiersze ----
 zakres <- 1:732
 wiersze <- vector("list", max(zakres))
-
 for(i in zakres) {
   strona_z_wierszem <- read_html(lista_linkow[i])
   wiersz <- strona_z_wierszem %>%
@@ -30,10 +29,9 @@ for(i in zakres) {
   wiersze[[i]] <- wiersz
 }
 save(wiersze, file = "wiersze.rda")
-
+# Daty napisania wierszy ----
 daty <- vector("list", max(zakres))
-
-for(i in 732) {
+for(i in zakres) {
   data_proba <- read_html(lista_linkow[[i]])
   tmp <- data_proba %>%
     html_nodes(css = ".fusion-text")
@@ -41,6 +39,38 @@ for(i in 732) {
   cat(".")
 }
 save(daty, file = "daty.rda")
-
-x <- html_text(daty[[70]])
-str_extract_all(x, "[0-9]{4}")
+## Zamiana dat na tekst
+daty_tekst <- lapply(daty, html_text)
+save(daty_tekst, file = "daty_tekst.rda")
+## Sam rok
+daty_rok <- lapply(daty_tekst, function(x) str_extract_all(x, "[0-9]{4}"))
+daty_rok_vec <- unlist(daty_rok, recursive = F)
+daty_rok_vec <- lapply(daty_rok, function(x) x[1])
+save(daty_rok, file = "daty_rok.rda")
+## Zastąpienie podwójnych lat jedną datą, brakujących NA
+# hist(unlist(sapply(daty_rok_vec, function(x) length(x[[1]]))))
+# podwojne_lata <- daty_rok_vec[unlist(sapply(daty_rok_vec, function(x) length(x[[1]]) == 2))]
+# which(sapply(daty_rok_vec, function(x) length(x[[1]]) == 2))
+daty_rok_vec <- lapply(daty_rok_vec, function(x) {
+  if(length(x[[1]]) > 0) {
+    x[[1]][1]
+  } else {
+    list(NA)
+  }
+})
+daty_rok_tib <- vector("list", 732)
+for(i in 1:732) {
+  daty_rok_tib[[i]] <- tibble(id = i, rok = daty_rok_vec[[i]][[1]])
+}
+lata_tib <- bind_rows(daty_rok_tib)
+save(lata_tib, file = "lata_tib.rda")
+# Połączenie informacji ----
+wiersze_tib <- vector("list", 732)
+for(i in 1:732) {
+  wiersze_tib[[i]] <- tibble(id = i, wiersz = wiersze[[i]])
+}
+wiersze_tib <- bind_rows(wiersze_tib)
+calosc <- left_join(lata_tib, wiersze_tib, by = "id")
+tytuly_tib <- tibble(id = 1:732, tytul = tytuly)
+calosc <- left_join(calosc, tytuly_tib, by = "id")
+save(calosc, file = "calosc.rda")
--- 1_2_obrobka_tekstow.R
+++ 1_2_obrobka_tekstow.R
@@ -1,18 +1,18 @@
 library(stringr)
 library(rvest)
 library(tidyverse)
-load("wiersze.rda")
-load("tytuly.rda")
-load("daty.rda")
-# Usunięcie nawiasów z przodu
-wiersze2 <- lapply(wiersze, function(x) x)
+load("calosc.rda")
+calosc <- filter(calosc, !is.na(wiersz))
+wiersze <- as.list(calosc$wiersz)
+# # Usunięcie nawiasów z przodu
+# wiersze2 <- lapply(wiersze, function(x) x)
 # Usunięcie znaków przestankowych
-wiersze_bez_przestankowych <- lapply(wiersze2, function(x) str_replace_all(x, "-", " "))
+wiersze_bez_przestankowych <- lapply(wiersze, function(x) str_replace_all(x, "-", " "))
 wiersze_bez_przestankowych <- lapply(wiersze_bez_przestankowych, function(x) str_replace_all(x, "[:punct:]+", ""))
 # Sprowadzenie wszystkich literów do małych liter
 wiersze_male_litery <- lapply(wiersze_bez_przestankowych, str_to_lower)
 wiersze_male_litery[[100]]
-# Podział na słowa
+# Podział na słowa ze zignorowaniem lematyzacji
 wiersze_slowa <- lapply(wiersze_male_litery, function(x) str_split(x, " "))
 # Lematyzacja
 library(httr)
@@ -24,72 +24,21 @@ getLemma <- function(t,u) {
   r <- gsub('[[:punct:] ]+','',unlist(as_list(xml_find_all(read_xml(r),"//base"))))
   return(r[r != ""])
 }
-proba_lemma <- getLemma(wiersze_male_litery[[1]], "mateusz.staniak@math.uni.wroc.pl")
-proba_lemma
-wiersze_lematyzacja <- vector("list", 732)
-for(i in 80:732) {
+wiersze_lematyzacja <- vector("list", length(wiersze))
+for(i in 1:length(wiersze)) {
   wiersze_lematyzacja[[i]] <- try(getLemma(wiersze_male_litery[[i]], "mateusz.staniak@math.uni.wroc.pl"))
 }
 save(wiersze_lematyzacja, file = "wiersze_lematyzacja.rda")
+load("wiersze_lematyzacja.rda")
 # Usunięcie słów stopu
-stopwords <- c("a", "aby", "ale", "bardziej", "bardzo", "bez", "bo", "bowiem", "był", "była",
-               "było", "były", "będzie", "co", "czy", "czyli", "dla", "dlatego", "do", "gdy",
-               "gdzie", "go", "i", "ich", "im", "innych", "iż", "jak", "jako", "jednak", "jego",
-               "jej", "jest", "jeszcze", "jeśli", "już", "kiedy", "kilka", "która", "które", "którego",
-               "której", "który", "których", "którym", "którzy", "lub", "ma", "mi", "między", "mnie",
-               "mogą", "może", "można", "na", "nad", "nam", "nas", "naszego", "naszych", "nawet", "nich",
-               "nie", "nim", "niż", "o", "od", "oraz", "po", "pod", "poza", "przed", "przede", "przez",
-               "przy", "również", "się", "sobie", "swoje", "są", "ta", "tak", "takie", "także", "tam",
-               "te", "tego", "tej", "ten", "też", "to", "tu", "tych", "tylko", "tym", "u", "w", "we", "wiele",
-               "wielu", "więc", "wszystkich", "wszystkim", "wszystko", "właśnie", "z", "za", "zawsze", "ze", "że")
-# Pobrac wiekszy zestaw
-stop_words_link <- "https://www.ranks.nl/stopwords/polish"
-wiersze_bez_stopu <- lapply(wiersze_lematyzacja, function(x) x[!(x %in% stopwords)])
-# wiersze_bez_stopu <- lapply(wiersze_lematyzacja1_79, function(x) setdiff(x, stopwords))
-wiersze_bez_stopu[[2]]
+stop_words <- unlist(jsonlite::read_json("https://raw.githubusercontent.com/stopwords-iso/stopwords-pl/master/stopwords-pl.json"))
+# wiersze_bez_stopu <- lapply(wiersze_lematyzacja, function(x) x[!(x %in% stopwords)])
+wiersze_bez_stopu_unikalne <- lapply(wiersze_lematyzacja, function(x) setdiff(x, stop_words))
+wiersze_bez_stopu <- lapply(wiersze_lematyzacja, function(x) x[!(x %in% stop_words)])
+save(wiersze_bez_stopu_unikalne, file = "wiersze_bez_stopu_unikalne.rda")
+save(wiersze_bez_stopu, file = "wiersze_bez_stopu.rda")
 library(wordcloud)
 czeste_slowa <- as.data.frame(table(unlist(wiersze_bez_stopu))) %>%
   arrange(desc(Freq))
+head(czeste_slowa, 20)
 save(czeste_slowa, file = "czeste_slowa.rda")
-### Czy daty są dobrze ściągnięte?
-daty_tekst <- lapply(daty, html_text)
-same_daty <- str_extract_all(daty_tekst, "[0-9]{4}")
-tibble(x = unlist(daty_tekst)) -> proba
-table(unlist(lapply(same_daty, length)))
-save(daty_tekst, file = "daty_tekst.rda")
-# same_daty[unlist(lapply(same_daty, length)) == 2]
-# tytuly[unlist(lapply(same_daty, length)) == 2]
-daty_z_na <- lapply(same_daty, function(x) if(length(x) == 0) NA else min(x))
-daty_z_na <- unlist(daty_z_na, use.names = FALSE)
-tworczosc <- tibble(tytul = tytuly, rok = daty_z_na)
-
-tworczosc2 <- tworczosc %>%
-  filter(tytul != "Mur",
-         !str_detect(tworczosc$tytul, "wiadomości"),
-         !str_detect(tworczosc$tytul, "wątpliwe"),
-         !str_detect(tworczosc$tytul, "dramat na "),
-         !str_detect(tworczosc$tytul, "Na rusztach"),
-         !str_detect(tworczosc$tytul, "Słowo o Bruno"),
-         !str_detect(tworczosc$tytul, "Estaca"),
-         !str_detect(tworczosc$tytul, "The Stake"),
-         !str_detect(tworczosc$tytul, "szkic"))
-
-
-czy_brak_daty <- unlist(lapply(same_daty, function(x) length(x) == 0))
-lista_linkow[czy_brak_daty]
-
-wszystkie_nietypowe <- which(czy_brak_daty)[c(1:4, 6, 15, 16)]
-do_usuniecia <- which(czy_brak_daty)[-c(1:4, 6, 15, 16)]
-
-same_daty[do_usuniecia] <- NULL
-tytuly[do_usuniecia] <- NULL
-
-
-
-lista_linkow[do_usuniecia]
-wiersze[do_usuniecia] <- NULL
-tytuly[do_usuniecia] <- NULL
-daty[do_usuniecia] <- NULL
-# same_daty[czy_brak_daty] <- NA
-# same_daty <- unlist(same_daty, use.names = FALSE)
-grep("Niech", tytuly, value = T)
